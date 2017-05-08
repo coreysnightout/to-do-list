5fd8035a1837d2d8797266bf19010fa4d0dff726
Brendan Hanna and Corey Redding <brendan@>
2017-05-08 16:19:48 -0700

    exchange form reset button for automatic form-clearing with js, add styling, header image.  add strike out styling to items as they appear on completed tasks. make it so bullet point from list items disappears from to do list

--- to-do/js/scripts.js
+++ to-do/js/scripts.js
@@ -1,15 +1,28 @@
+// Back End //
+function Chore(name) {
+  this.choreName = name;
+  this.completed = false;
+};
+
+Chore.prototype.crossOut = function() {
+  return this.choreName;
+}
+
+
 $(document).ready(function() {
   $("form#to-do-form").submit(function(event) {
     event.preventDefault();
 
     var thingToDo =$("input#thing-to-do").val();
 
+    var newChore = new Chore(thingToDo);
 
-    $("ul#where-things-go").append("<li><span class='clickme'>" + thingToDo + "</span></li>");
+
+    $("ul#where-things-go").append("<li><span class='clickme'>" + newChore.choreName + "</span></li>");
 
     $(".clickme").last().click(function() {
-      $("#completed-thing").append("<li>" + thingToDo + "</li>");
-      $(this).fadeOut();
+      $("#completed-thing").append("<li style='text-decoration: line-through;'>" + newChore.crossOut() + "</li>");
+      $(this).parent().fadeOut();
 
     //
     // $("#completed-thing").last().click(function() {
@@ -19,9 +32,11 @@ $(document).ready(function() {
 
     });
 
-    $(".reset").click(function(event) {
-      form.reset();
+    document.forms["to-do-form"].reset();
 
-    });
+    // $(".reset").click(function(event) {
+    //   form.reset();
+    //
+    // });
   });
 });
